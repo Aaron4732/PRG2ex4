99712b0984add1b2b1c89ab22635e13e17a05c0b
Fries Aaron-Dario <113194397+Aaron4732@users.noreply.github.com>
2023-06-12 19:59:09 +0200

    add builder patter

--- src/main/java/at/ac/fhcampuswien/fhmdb/api/MovieAPI.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/api/MovieAPI.java
@@ -14,6 +14,20 @@ public class MovieAPI {
     private static final String URL = "http://prog2.fh-campuswien.ac.at/movies"; // https if certificates work
     private static final OkHttpClient client = new OkHttpClient();
 
+    private String base;
+    private String query;
+    private Genre genre;
+    private String releaseYear;
+    private String ratingFrom;
+
+    public MovieAPI(MovieAPIBuilder builder) {
+        this.base = builder.base;
+        this.query = builder.query;
+        this.genre = builder.genre;
+        this.releaseYear = builder.releaseYear;
+        this.ratingFrom = builder.ratingFrom;
+    }
+
     private String buildUrl(UUID id) {
         StringBuilder url = new StringBuilder(URL);
         if (id != null) {
@@ -24,7 +38,6 @@ public class MovieAPI {
 
     private static String buildUrl(String query, Genre genre, String releaseYear, String ratingFrom) {
         StringBuilder url = new StringBuilder(URL);
-
         if ( (query != null && !query.isEmpty()) ||
                 genre != null || releaseYear != null || ratingFrom != null) {
 
@@ -53,7 +66,14 @@ public class MovieAPI {
     }
 
     public static List<Movie> getAllMovies(String query, Genre genre, String releaseYear, String ratingFrom) throws MovieApiException{
-        String url = buildUrl(query, genre, releaseYear, ratingFrom);
+        //String url = buildUrl(query, genre, releaseYear, ratingFrom);
+        String url = new MovieAPIBuilder(URL)
+                .query(query)
+                .genre(genre)
+                .releaseYear(releaseYear)
+                .ratingFrom(ratingFrom)
+                .buildUrl();
+
         Request request = new Request.Builder()
                 .url(url)
                 .removeHeader("User-Agent")
@@ -82,6 +102,65 @@ public class MovieAPI {
             return gson.fromJson(Objects.requireNonNull(response.body()).string(), Movie.class);
         } catch (Exception e) {
             throw new MovieApiException(e.getMessage());
+        }
+    }
+    public static class MovieAPIBuilder {
+
+        public static final String DELIMITER = "&";
+        private final String base;
+        private String query;
+        private Genre genre;
+        private String releaseYear;
+        private String ratingFrom;
+
+        public MovieAPIBuilder(String base) {
+            this.base = base;
+        }
+
+        public MovieAPIBuilder query(String query) {
+            this.query = query;
+            return this;
+        }
+
+        public MovieAPIBuilder genre(Genre genre) {
+            this.genre = genre;
+            return this;
+        }
+
+        public MovieAPIBuilder releaseYear(String releaseYear) {
+            this.releaseYear = releaseYear;
+            return this;
+        }
+
+        public MovieAPIBuilder ratingFrom(String ratingFrom) {
+            this.ratingFrom = ratingFrom;
+            return this;
+        }
+
+        private String buildUrl() {
+            StringBuilder url = new StringBuilder(this.base);
+
+            if ((query != null && !query.isEmpty()) ||
+                    genre != null || releaseYear != null || ratingFrom != null) {
+
+                url.append("?");
+
+                // check all parameters and add them to the url
+                if (query != null && !query.isEmpty()) {
+                    url.append("query=").append(query).append(DELIMITER);
+                }
+                if (genre != null) {
+                    url.append("genre=").append(genre).append(DELIMITER);
+                }
+                if (releaseYear != null) {
+                    url.append("releaseYear=").append(releaseYear).append(DELIMITER);
+                }
+                if (ratingFrom != null) {
+                    url.append("ratingFrom=").append(ratingFrom).append(DELIMITER);
+                }
+            }
+
+            return url.toString();
         }
     }
 }
