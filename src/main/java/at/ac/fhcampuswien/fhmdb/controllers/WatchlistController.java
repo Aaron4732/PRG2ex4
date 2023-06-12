921c6c154ab2db4f33b2586e849e0142e79e59eb
beatrixdietl <113309245+beatrixdietl@users.noreply.github.com>
2023-06-12 17:41:10 +0200

    WatchlistController und WatchlistController

--- src/main/java/at/ac/fhcampuswien/fhmdb/controllers/WatchlistController.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/controllers/WatchlistController.java
@@ -7,17 +7,18 @@ import at.ac.fhcampuswien.fhmdb.database.WatchlistRepository;
 import at.ac.fhcampuswien.fhmdb.ui.UserDialog;
 import at.ac.fhcampuswien.fhmdb.ui.WatchlistCell;
 import com.jfoenix.controls.JFXListView;
+import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.ResourceBundle;
+import at.ac.fhcampuswien.fhmdb.observer.Observer;
 
-public class WatchlistController implements Initializable {
+import java.net.URL;
+import java.util.*;
+
+public class WatchlistController implements Initializable, Observer {
 
     @FXML
     public JFXListView watchlistView;
@@ -42,17 +43,18 @@ public class WatchlistController implements Initializable {
         }
     };
 
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
         List<WatchlistMovieEntity> watchlist = new ArrayList<>();
         try {
             watchlistRepository = new WatchlistRepository();
+            watchlistRepository.attach(this);
             watchlist = getWatchlist();
             observableWatchlist.addAll(getWatchlist());
             watchlistView.setItems(observableWatchlist);
             watchlistView.setCellFactory(movieListView -> new WatchlistCell(onRemoveFromWatchlistClicked));
-
         } catch (DataBaseException e) {
             UserDialog dialog = new UserDialog("Database Error", "Could not read movies from DB");
             dialog.show();
@@ -69,5 +71,14 @@ public class WatchlistController implements Initializable {
 
     private List<WatchlistMovieEntity> getWatchlist() throws DataBaseException {
         return watchlistRepository.readWatchlist();
+    }
+
+    @Override
+    public void update(WatchlistMovieEntity movie) {
+        Platform.runLater(() -> {
+            System.out.println("Movie added: " + movie.getTitle());
+            UserDialog dialog = new UserDialog("Movie Added", "Movie \"" + movie.getTitle() + "\" has been added to the watchlist.");
+            dialog.show();
+        });
     }
 }
--- src/main/java/at/ac/fhcampuswien/fhmdb/database/WatchlistRepository.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/database/WatchlistRepository.java
@@ -1,18 +1,43 @@
 package at.ac.fhcampuswien.fhmdb.database;
 
+import at.ac.fhcampuswien.fhmdb.observer.Observable;
+import at.ac.fhcampuswien.fhmdb.observer.Observer;
+import at.ac.fhcampuswien.fhmdb.ui.UserDialog;
 import com.j256.ormlite.dao.Dao;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Consumer;
 
-public class WatchlistRepository {
+public class WatchlistRepository implements Observable {
+
 
     Dao<WatchlistMovieEntity, Long> dao;
+    private List<Observer> observers;
 
     public WatchlistRepository() throws DataBaseException {
         try {
             this.dao = DatabaseManager.getInstance().getWatchlistDao();
         } catch (Exception e) {
             throw new DataBaseException(e.getMessage());
+        }
+        this.observers = new ArrayList<>();
+    }
+
+    @Override
+    public void attach(Observer observer) {
+        observers.add(observer);
+    }
+
+    @Override
+    public void detach(Observer observer) {
+        observers.remove(observer);
+    }
+
+    @Override
+    public void notifyObservers(WatchlistMovieEntity movie) {
+        for (Observer observer : observers) {
+            observer.update(movie);
         }
     }
 
@@ -28,8 +53,16 @@ public class WatchlistRepository {
         try {
             // only add movie if it does not exist yet
             long count = dao.queryBuilder().where().eq("apiId", movie.getApiId()).countOf();
-            if (count == 0) {
+            if (count > 0 ) {
+                UserDialog dialog = new UserDialog("Movie Already Added", "The movie \"" + movie.getTitle() + "\" is already in the watchlist.");
+                dialog.show();
+                System.out.println("Movie is already in Watchlist");
+            } else if (count == 0) {
                 dao.create(movie);
+                notifyObservers(movie);
+                UserDialog dialog = new UserDialog("Movie Successfully Added", "The movie \"" + movie.getTitle() + "\" has successfully been added to the watchlist.");
+                dialog.show();
+                System.out.println("Movie was added to Watchlist");
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -52,4 +85,5 @@ public class WatchlistRepository {
             throw new DataBaseException("Error while checking if movie is on watchlist");
         }
     }
+
 }
